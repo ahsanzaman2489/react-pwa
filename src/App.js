433b963251597a7165d91c022c144c55ead64512
ahsanzaman2489 <ahsan_zami@hotmail.com>
2019-05-16 15:28:55 +0500

    Home page and redux init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import Header from "./components/header";
+// import Footer from "./components/footer";
 
-export default App;
+import HomePage from "./pages/Home";
+
+
+export default () => {
+    return (
+        <div>
+            {/*<Header src={logo}/>*/}
+            <Router>
+                <Switch>
+                    <Redirect exact from="/" to="/Home"/>
+                    <Route exact path={'/Home'} component={HomePage}/>
+                    {/*<Route path={'/cars/detail/:stockNumber'} component={CarDetailContainer}/>*/}
+                    {/*<Route path="*" component={NotFoundComponent}/>*/}
+                </Switch>
+                {/*<Footer/>*/}
+            </Router>
+        </div>
+    );
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as NewsActions from '../actions/News';
+
+// import {NO_DATA} from '../../constants/app';
+
+
+export class HomePage extends Component {
+    state = {
+
+    };
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+
+        return (
+
+            <div className="car-detail">
+                Home
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        car: state,
+    };
+};
+
+function mapDispatchToProps(dispatch) {
+    return {...bindActionCreators({NewsActions}, dispatch)}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
